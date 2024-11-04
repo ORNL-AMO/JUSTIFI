2d6080048e9aeae29047d9b81acaa1430122afd8
rmroot <rootrm@ornl.gov>
2024-11-04 11:00:55 -0600

    all tests passing

--- src/app/core-components/toast-notifications/toast-notifications.service.spec.ts
+++ src/app/core-components/toast-notifications/toast-notifications.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToastNotificationsService } from './toast-notifications.service';
+import { LocalStorageDataService } from 'src/app/shared/shared-services/local-storage-data.service';
 
 describe('ToastNotificationsService', () => {
   let service: ToastNotificationsService;
 
+  let localStorageDataService: Partial<LocalStorageDataService> = {}
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: LocalStorageDataService, useValue: localStorageDataService }
+      ]
+    });
     service = TestBed.inject(ToastNotificationsService);
   });
 
