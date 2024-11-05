5d8826a00b8cafdbad57055c61995bd2b6c0b75c
rmroot <rootrm@ornl.gov>
2024-11-05 08:36:50 -0600

    all tests passing

--- src/app/setup-wizard/pre-visit/company-kpi-details/kpm-database-modal/kpm-database-modal.component.spec.ts
+++ src/app/setup-wizard/pre-visit/company-kpi-details/kpm-database-modal/kpm-database-modal.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KpmDatabaseModalComponent } from './kpm-database-modal.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { getNewKeyPerformanceIndicator } from 'src/app/models/keyPerformanceIndicator';
+import { KeyPerformanceIndicatorOption } from 'src/app/shared/constants/keyPerformanceIndicatorOptions';
 
 describe('KpmDatabaseModalComponent', () => {
   let component: KpmDatabaseModalComponent;
@@ -8,12 +11,20 @@ describe('KpmDatabaseModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FontAwesomeModule],
       declarations: [KpmDatabaseModalComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(KpmDatabaseModalComponent);
     component = fixture.componentInstance;
+    let tmpIndicatorOption: KeyPerformanceIndicatorOption = {
+      primaryKPI: 'Operations',
+      label: '',
+      htmlLabel: '',
+      optionValue: 'chemicalEmissions'
+    }
+    component.keyPerformanceIndicator = getNewKeyPerformanceIndicator('', '', tmpIndicatorOption, false)
     fixture.detectChanges();
   });
 
