e80dace5c1aa13e9cedfb9c96d455a77008c4832
rmroot <rootrm@ornl.gov>
2024-11-15 13:52:43 -0600

    all tests passing

--- src/app/shared/shared-assessment-forms/energy-opportunity-nebs-table/energy-opportunity-nebs-table.component.spec.ts
+++ src/app/shared/shared-assessment-forms/energy-opportunity-nebs-table/energy-opportunity-nebs-table.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnergyOpportunityNebsTableComponent } from './energy-opportunity-nebs-table.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HelperPipesModule } from '../../helper-pipes/_helper-pipes.module';
+import { stubServiceProviders } from 'src/app/spec-helpers/spec-test-service-stub';
 
 describe('EnergyOpportunityNebsTableComponent', () => {
   let component: EnergyOpportunityNebsTableComponent;
@@ -8,7 +11,9 @@ describe('EnergyOpportunityNebsTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EnergyOpportunityNebsTableComponent]
+      imports: [FontAwesomeModule, HelperPipesModule],
+      declarations: [EnergyOpportunityNebsTableComponent],
+      providers: stubServiceProviders
     })
     .compileComponents();
 
