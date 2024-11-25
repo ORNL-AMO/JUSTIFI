f1f845ab5a641ae6746e67e882041c829c04f43f
rmroot <rootrm@ornl.gov>
2024-11-25 10:47:33 -0600

    updated kpm and kpis for emissions

--- src/app/shared/constants/keyPerformanceMetrics.ts
+++ src/app/shared/constants/keyPerformanceMetrics.ts
@@ -83,7 +83,6 @@ export type KeyPerformanceMetricValue =
     'percentTotalOrCost' |
     'consumptionCostWater' |
     'consumptionCostSewage' |
-    'percentOrTotalChemicalEmissions' |
     'percentOrTotalRefrigerantEmissions' |
     'TRIR' |
     'oshaRecordableIncidents' |
@@ -104,7 +103,14 @@ export type KeyPerformanceMetricValue =
     'treatmentChemicals' |
     'rawMaterials' |
     'intermediateGoods' |
-    'custom';
+    'custom' |
+    'scope1Emissions' |
+    'scope2Emissions' |
+    'scope3Emissions' |
+    'percentTestsMeetingStandardsAirPollutants' |
+    'noxSoxCoEmissions' |
+    'particulateEmissions' |
+    'percentTestsMeetingStandardsWaterPollutants';
 
 
 export type KpmCalculationMethod = 'costPerUnit' | 'percentTotal' | 'directCost';
@@ -534,10 +540,70 @@ export const KeyPerformanceMetricOptions: Array<KeyPerformanceMetricOption> = [
         calculationMethod: 'percentTotal'
     },
     {
-        label: "Quantity",
-        htmlLabel: "Quantity",
-        value: "percentOrTotalChemicalEmissions",
+        label: "Scope 1 Emissions",
+        htmlLabel: "Scope 1 Emissions",
+        value: "scope1Emissions",
         kpiValue: "chemicalEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "Scope 2 Emissions",
+        htmlLabel: "Scope 2 Emissions",
+        value: "scope2Emissions",
+        kpiValue: "chemicalEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "Scope 3 Emissions",
+        htmlLabel: "Scope 3 Emissions",
+        value: "scope3Emissions",
+        kpiValue: "chemicalEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "Particulate Emissions",
+        htmlLabel: "Particulate Emissions",
+        value: "particulateEmissions",
+        kpiValue: "airPollutantEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "NOx, SOx, CO Emissions",
+        htmlLabel: "NOx, SOx, CO Emissions",
+        value: "noxSoxCoEmissions",
+        kpiValue: "airPollutantEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "Percent of tests that meet regulatory standards",
+        htmlLabel: "Percent of tests that meet regulatory standards",
+        value: "percentTestsMeetingStandardsAirPollutants",
+        kpiValue: "airPollutantEmissions",
+        isQuantitative: true,
+        goalToIncrease: false,
+        timePeriod: 'yr',
+        calculationMethod: 'percentTotal'
+    },
+    {
+        label: "Percent of tests that meet regulatory standards",
+        htmlLabel: "Percent of tests that meet regulatory standards",
+        value: "percentTestsMeetingStandardsWaterPollutants",
+        kpiValue: "waterPollutantEmissions",
         isQuantitative: true,
         goalToIncrease: false,
         timePeriod: 'yr',
