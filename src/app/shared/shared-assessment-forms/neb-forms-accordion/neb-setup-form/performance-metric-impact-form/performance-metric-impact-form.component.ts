b455b5bffd9d31b80552799ce3aa3c4f7c3387b7
rmroot <rootrm@ornl.gov>
2025-02-26 11:19:25 -0600

    fixes to kpm impacts form

--- src/app/shared/shared-assessment-forms/neb-forms-accordion/neb-setup-form/performance-metric-impact-form/performance-metric-impact-form.component.ts
+++ src/app/shared/shared-assessment-forms/neb-forms-accordion/neb-setup-form/performance-metric-impact-form/performance-metric-impact-form.component.ts
@@ -61,7 +61,7 @@ export class PerformanceMetricImpactFormComponent {
       if (!this.isFormChange) {
         this.keyPerformanceMetricImpact = this.keyPerformanceMetricImpactIdbService.getByGuid(this.impactGuid);
         if (this.keyPerformanceMetricImpact) {
-          this.keyPerformanceMetric = this.keyPerformanceIndicatorIdbService.getKeyPerformanceMetric(this.keyPerformanceMetricImpact.companyId, this.keyPerformanceMetricImpact.kpmGuid);
+          this.keyPerformanceMetric = this.keyPerformanceIndicatorIdbService.getKeyPerformanceMetric(this.keyPerformanceMetricImpact.facilityId, this.keyPerformanceMetricImpact.kpmGuid);
         }
       } else {
         this.isFormChange = false;
