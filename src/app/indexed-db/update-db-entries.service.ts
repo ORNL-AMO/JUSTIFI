cef4c96daed0a45349bd7182ae119cf0948c3e45
Roy <Roy.Li@nrel.gov>
2025-03-21 14:03:29 -0600

    PR review: add utility category for EEO in db entries change

--- src/app/indexed-db/update-db-entries.service.ts
+++ src/app/indexed-db/update-db-entries.service.ts
@@ -20,6 +20,7 @@ import { IdbEnergyEquipment } from '../models/energyEquipment';
 import { updateAssessmentUtilityUseCost, updateFacilityUtilityUseCost } from '../shared/reports/calculations/utilityCalculation';
 import { AssessmentIdbService } from './assessment-idb.service';
 import { IdbAssessment } from '../models/assessment';
+import { EnergyOpportunityIdbService } from './energy-opportunity-idb.service';
 
 @Injectable({
   providedIn: 'root'
@@ -35,7 +36,8 @@ export class UpdateDbEntriesService {
     private userIdbService: UserIdbService,
     private localeService: LocaleService,
     private processEquipmentIdbService: ProcessEquipmentIdbService,
-    private energyEquipmentIdbService: EnergyEquipmentIdbService
+    private energyEquipmentIdbService: EnergyEquipmentIdbService,
+    private energyOpportunityIdbService: EnergyOpportunityIdbService,
   ) { }
 
   async updateDbEntries(user: IdbUser): Promise<IdbUser> {
@@ -54,9 +56,10 @@ export class UpdateDbEntriesService {
 
     await this.updateProcessEquipment();
     await this.updateEnergyEquipment();
-    await this.updateCompany();
-    await this.updateFacility();
-    await this.updateAssessment();
+    await this.updateCompanies();
+    await this.updateFacilities();
+    await this.updateAssessments();
+    await this.updateEnergyOpportunities();
     
     if (userNeedsUpdate) {
       user = await firstValueFrom(this.userIdbService.updateWithObservable(user));
@@ -170,7 +173,7 @@ export class UpdateDbEntriesService {
     }
   }
 
-  async updateCompany() {
+  async updateCompanies() {
     let companies: Array<IdbCompany> = await firstValueFrom(this.companyIdbService.getAll());
     // back-compatibility for company energy unit
     for (let i = 0; i < companies.length; i++) {
@@ -182,7 +185,7 @@ export class UpdateDbEntriesService {
     }
   }
 
-  async updateFacility() {
+  async updateFacilities() {
     let facilities: Array<IdbFacility> = await firstValueFrom(this.facilityIdbService.getAll());
     // Get company entries
     let companies: Array<IdbCompany> = await firstValueFrom(this.companyIdbService.getAll());
@@ -198,7 +201,7 @@ export class UpdateDbEntriesService {
     }
   }
   
-  async updateAssessment() {
+  async updateAssessments() {
     let assessments: Array<IdbAssessment> = await firstValueFrom(this.assessmentIdbService.getAll());
     let facilities: Array<IdbFacility> = await firstValueFrom(this.facilityIdbService.getAll());
     let companies: Array<IdbCompany> = await firstValueFrom(this.companyIdbService.getAll());
@@ -215,6 +218,23 @@ export class UpdateDbEntriesService {
         }
       }
       await firstValueFrom(this.assessmentIdbService.updateWithObservable(assessment));
+    }
+  }
+
+  async updateEnergyOpportunities() {
+    let energyOpportunities = await firstValueFrom(this.energyOpportunityIdbService.getAll());
+    // back-compatibility for utilityCategory
+    for (let i = 0; i < energyOpportunities.length; i++) {
+      let energyOpportunity = energyOpportunities[i];
+      if (energyOpportunity.utilityCategory == undefined) {
+        // need update the utilityCategory to energy or water
+        if (energyOpportunity.utilityType === 'Water' || energyOpportunity.utilityType === 'Waste Water') {
+          energyOpportunity.utilityCategory = 'water';
+        } else {
+          energyOpportunity.utilityCategory = 'energy';
+        }
+        await firstValueFrom(this.energyOpportunityIdbService.updateWithObservable(energyOpportunity));
+      }
     }
   }
 }
