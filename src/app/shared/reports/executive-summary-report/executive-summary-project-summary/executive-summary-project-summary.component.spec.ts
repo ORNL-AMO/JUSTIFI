003e300729ded831a3be113260ff129a0632891c
Roy <Roy.Li@nrel.gov>
2025-04-21 15:28:22 -0600

    unit test

--- src/app/shared/reports/executive-summary-report/executive-summary-project-summary/executive-summary-project-summary.component.spec.ts
+++ src/app/shared/reports/executive-summary-report/executive-summary-project-summary/executive-summary-project-summary.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExecutiveSummaryProjectSummaryComponent } from './executive-summary-project-summary.component';
+import { stubServiceProviders } from 'src/app/spec-helpers/spec-test-service-stub';
+import { ExecutiveSummaryReport, getExecutiveSummaryReport } from '../../calculations/executiveSummaryReport';
 
 describe('ExecutiveSummaryProjectSummaryComponent', () => {
   let component: ExecutiveSummaryProjectSummaryComponent;
@@ -8,12 +10,14 @@ describe('ExecutiveSummaryProjectSummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ExecutiveSummaryProjectSummaryComponent]
+      declarations: [ExecutiveSummaryProjectSummaryComponent],
+      providers: stubServiceProviders,
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ExecutiveSummaryProjectSummaryComponent);
     component = fixture.componentInstance;
+    component.executiveSummaryReport = getExecutiveSummaryReport(new Date(), [], [], [], [], [], []);
     fixture.detectChanges();
   });
 
