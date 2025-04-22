6cd567e2ac25408e52b08dbe9025177e0a5e279e
Roy <Roy.Li@nrel.gov>
2025-04-22 15:36:00 -0600

    rename the files and variables

--- src/app/shared/reports/executive-summary-report/executive-summary-kpi-impacts/executive-summary-kpi-impacts.component.spec.ts
+++ src/app/shared/reports/executive-summary-report/executive-summary-kpi-impacts/executive-summary-kpi-impacts.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ExecutiveSummaryKpmImpactsComponent } from './executive-summary-kpm-impacts.component';
+import { ExecutiveSummaryKpiImpactsComponent } from './executive-summary-kpi-impacts.component';
 import { stubServiceProviders } from 'src/app/spec-helpers/spec-test-service-stub';
 import { CurrencySymbolPipe } from 'src/app/shared/helper-pipes/currency-symbol.pipe';
 import { CurrencyPipe } from '@angular/common';
@@ -9,8 +9,8 @@ import { TableEntriesModule } from 'src/app/shared/table-entries/table-entries.m
 import { HelperPipesModule } from 'src/app/shared/helper-pipes/_helper-pipes.module';
 
 describe('ExecutiveSummaryKpmImpactsComponent', () => {
-  let component: ExecutiveSummaryKpmImpactsComponent;
-  let fixture: ComponentFixture<ExecutiveSummaryKpmImpactsComponent>;
+  let component: ExecutiveSummaryKpiImpactsComponent;
+  let fixture: ComponentFixture<ExecutiveSummaryKpiImpactsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,7 +19,7 @@ describe('ExecutiveSummaryKpmImpactsComponent', () => {
         HelperPipesModule,
       ],
       declarations: [
-        ExecutiveSummaryKpmImpactsComponent,
+        ExecutiveSummaryKpiImpactsComponent,
         CurrencySymbolPipe,
       ],
       providers: [
@@ -29,7 +29,7 @@ describe('ExecutiveSummaryKpmImpactsComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ExecutiveSummaryKpmImpactsComponent);
+    fixture = TestBed.createComponent(ExecutiveSummaryKpiImpactsComponent);
     component = fixture.componentInstance;
     component.executiveSummaryReport = getExecutiveSummaryReport(new Date(), [], [], [], [], [], [], []);
     fixture.detectChanges();
