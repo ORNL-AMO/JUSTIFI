36f9199a9d840478a77dafdff2564572bf5eb4a4
Roy <Roy.Li@nrel.gov>
2025-04-23 14:11:30 -0600

    sorting to project summary table

--- src/app/shared/reports/executive-summary-report/executive-summary-project-summary/executive-summary-project-summary.component.ts
+++ src/app/shared/reports/executive-summary-report/executive-summary-project-summary/executive-summary-project-summary.component.ts
@@ -32,6 +32,9 @@ export class ExecutiveSummaryProjectSummaryComponent {
     totalFinancialImpact: 0,
   };
   topKpis: Array<KeyPerformanceIndicatorOption>;
+  limit: number = 4; // limit top KPIs to show
+  orderByField: 'UtilitySavings' | 'PaybackNoNeb' | 'FinancialImpact' | 'PaybackWNeb' = 'FinancialImpact';
+  orderByDir: 'asc' | 'desc' = 'desc';
 
   currencyCode: string;
   currencySub: Subscription;
@@ -41,20 +44,8 @@ export class ExecutiveSummaryProjectSummaryComponent {
   ) { }
 
   ngOnInit() {
-    // flatten all EEM reports
-    this.allEEMReports = this.executiveSummaryReport.assessmentReports.flatMap(
-      (assessmentReport: AssessmentReport) => {
-        return assessmentReport.energyOpportunityReports;
-      }
-    );
-    // aggregate EEM reports by cost savings
-    this.reducedEEMReports = this.orderReduceEEMReportsByCostSavings(this.allEEMReports, 4);
-    // filter top 3 KPIs
-    const kpiReportItems = this.executiveSummaryReport.keyPerformanceIndicatorReport.kpiReportItems;
-    kpiReportItems.sort((a, b) => {
-      return b.percentSavings - a.percentSavings;
-    });
-    this.topKpis = kpiReportItems.slice(0, 3).map(item => item.keyPerformanceIndicator);
+    // Set EEM Reports
+    this.setEEMReports();
     // get currency code and symbol
     this.currencySub = this.localeService.currencyCode.subscribe(
       code => {this.currencyCode = code}
@@ -65,10 +56,58 @@ export class ExecutiveSummaryProjectSummaryComponent {
     this.currencySub.unsubscribe();
   }
 
-  orderReduceEEMReportsByCostSavings(allEEMReports: Array<EnergyOpportunityReport>, limit: number): Array<EnergyOpportunityReport> {
-    allEEMReports.sort((a, b) => {
-      return b.totalFinancialImpact - a.totalFinancialImpact;
+  setEEMReports() {
+    // flatten all EEM reports
+    this.allEEMReports = this.executiveSummaryReport.assessmentReports.flatMap(
+      (assessmentReport: AssessmentReport) => {
+        return assessmentReport.energyOpportunityReports;
+      }
+    );
+    // sort and aggregate EEM reports
+    this.sortEEMReports(this.allEEMReports, this.orderByField, this.orderByDir);
+    this.reducedEEMReports = this.orderReduceEEMReportsByCostSavings(this.allEEMReports, this.limit);
+    // filter top 3 KPIs
+    const kpiReportItems = this.executiveSummaryReport.keyPerformanceIndicatorReport.kpiReportItems;
+    kpiReportItems.sort((a, b) => {
+      return b.percentSavings - a.percentSavings;
     });
+    this.topKpis = kpiReportItems.slice(0, this.limit - 1).map(item => item.keyPerformanceIndicator);
+  }
+
+  setOrderByField(orderByField: 'UtilitySavings' | 'PaybackNoNeb' | 'FinancialImpact' | 'PaybackWNeb') {
+    this.orderByField = orderByField;
+    if (orderByField == 'UtilitySavings') {
+      this.orderByDir = 'desc';
+    } else if (orderByField == 'PaybackNoNeb') {
+      this.orderByDir = 'asc';
+    } else if (orderByField == 'FinancialImpact') {
+      this.orderByDir = 'desc';
+    } else if (orderByField == 'PaybackWNeb') {
+      this.orderByDir = 'asc';
+    } else {
+      this.orderByDir = 'desc';
+    }
+    this.sortEEMReports(this.reducedEEMReports, orderByField, this.orderByDir);
+    this.reducedEEMReports = this.orderReduceEEMReportsByCostSavings(this.allEEMReports, this.limit);
+  }
+  sortEEMReports(EEMReports: Array<EnergyOpportunityReport>,
+    orderByField: 'UtilitySavings' | 'PaybackNoNeb' | 'FinancialImpact' | 'PaybackWNeb',
+    orderByDir: 'asc' | 'desc') {
+    EEMReports.sort((a, b) => {
+      if (orderByField == 'UtilitySavings') {
+        return orderByDir == 'asc' ? a.totalNonNebCostSavings - b.totalNonNebCostSavings : b.totalNonNebCostSavings - a.totalNonNebCostSavings;
+      } else if (orderByField == 'PaybackNoNeb') {
+        return orderByDir == 'asc' ? a.paybackWithoutNebs - b.paybackWithoutNebs : b.paybackWithoutNebs - a.paybackWithoutNebs;
+      } else if (orderByField == 'FinancialImpact') {
+        return orderByDir == 'asc' ? a.totalFinancialImpact - b.totalFinancialImpact : b.totalFinancialImpact - a.totalFinancialImpact;
+      } else if (orderByField == 'PaybackWNeb') {
+        return orderByDir == 'asc' ? a.paybackWithNebs - b.paybackWithNebs : b.paybackWithNebs - a.paybackWithNebs;
+      }
+      return 0;
+    });
+  }
+
+  orderReduceEEMReportsByCostSavings(allEEMReports: Array<EnergyOpportunityReport>, limit: number): Array<EnergyOpportunityReport> {
     if (allEEMReports.length > limit) {
       const topReports = allEEMReports.slice(0, limit - 1);
       const otherReports = allEEMReports.slice(limit - 1);
