5f035b647857e5e73dc5adca0f833ebebdba3ef2
rmroot <rootrm@ornl.gov>
2025-05-13 11:06:21 -0500

    all tests passing

--- src/app/shared/reports/additional-savings-message/additional-savings-message.component.spec.ts
+++ src/app/shared/reports/additional-savings-message/additional-savings-message.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdditionalSavingsMessageComponent } from './additional-savings-message.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TableEntriesModule } from '../../table-entries/table-entries.module';
+import { getAssessmentReport } from '../calculations/assessmentReport';
+import { getNewIdbAssessment, IdbAssessment } from 'src/app/models/assessment';
+import { getDefaultUnitSettings } from 'src/app/models/unitSettings';
 
 describe('AdditionalSavingsMessageComponent', () => {
   let component: AdditionalSavingsMessageComponent;
@@ -8,12 +13,16 @@ describe('AdditionalSavingsMessageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FontAwesomeModule, TableEntriesModule],
       declarations: [AdditionalSavingsMessageComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AdditionalSavingsMessageComponent);
     component = fixture.componentInstance;
+    component.context = 'assessment';
+    let assessment: IdbAssessment = getNewIdbAssessment('', '', '', getDefaultUnitSettings());
+    component.assessmentReport = getAssessmentReport(assessment, [], [], [], [], []);
     fixture.detectChanges();
   });
 
