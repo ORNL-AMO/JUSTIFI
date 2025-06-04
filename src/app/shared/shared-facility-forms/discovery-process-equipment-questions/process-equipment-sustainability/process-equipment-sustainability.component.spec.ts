6a9b581e337a88a0ceb8ec15a0fd1c6012174d74
Roy Li <roy.li@nrel.gov>
2025-06-04 10:50:21 -0600

    fix typo sustainablity

--- src/app/shared/shared-facility-forms/discovery-process-equipment-questions/process-equipment-sustainability/process-equipment-sustainability.component.spec.ts
+++ src/app/shared/shared-facility-forms/discovery-process-equipment-questions/process-equipment-sustainability/process-equipment-sustainability.component.spec.ts
@@ -1,23 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProcessEquipmentSustainablityComponent } from './process-equipment-sustainablity.component';
+import { ProcessEquipmentSustainabilityComponent } from './process-equipment-sustainability.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { stubServiceProviders } from 'src/app/spec-helpers/spec-test-service-stub';
 
-describe('ProcessEquipmentSustainablityComponent', () => {
-  let component: ProcessEquipmentSustainablityComponent;
-  let fixture: ComponentFixture<ProcessEquipmentSustainablityComponent>;
+describe('ProcessEquipmentSustainabilityComponent', () => {
+  let component: ProcessEquipmentSustainabilityComponent;
+  let fixture: ComponentFixture<ProcessEquipmentSustainabilityComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FontAwesomeModule, FormsModule],
-      declarations: [ProcessEquipmentSustainablityComponent],
+      declarations: [ProcessEquipmentSustainabilityComponent],
       providers: stubServiceProviders
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProcessEquipmentSustainablityComponent);
+    fixture = TestBed.createComponent(ProcessEquipmentSustainabilityComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
