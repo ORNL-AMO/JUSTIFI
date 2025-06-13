161a5838921420ea88f6396cc2157df926a17e5d
Roy Li <170121816+RLiNREL@users.noreply.github.com>
2025-06-13 15:23:53 -0600

    kpm search keyword and filter

--- src/app/shared/shared-assessment-forms/neb-forms-accordion/neb-setup-form/performance-metrics-modal/metric-options-modal-list.pipe.ts
+++ src/app/shared/shared-assessment-forms/neb-forms-accordion/neb-setup-form/performance-metrics-modal/metric-options-modal-list.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { KeyPerformanceMetric, KeyPerformanceMetricOption } from 'src/app/shared/constants/keyPerformanceMetrics';
+import { KeyPerformanceMetric, KeyPerformanceMetricOption, KpmKeywords } from 'src/app/shared/constants/keyPerformanceMetrics';
 import * as _ from 'lodash';
 
 @Pipe({
@@ -11,13 +11,18 @@ export class MetricOptionsModalListPipe implements PipeTransform {
   transform(options: Array<KeyPerformanceMetric>, searchStr: string, orderByDir: 'asc' | 'desc'): Array<KeyPerformanceMetric> {
     let filteredOptions: Array<KeyPerformanceMetric> = options;
     if (searchStr) {
-      filteredOptions = filteredOptions.filter(option => {
-        return option.label.toLowerCase().includes(searchStr.toLowerCase());
+      const search = searchStr.toLowerCase().trim();
+      let labelOptions = filteredOptions.filter(option => {
+        return option.label.toLowerCase().includes(search);
       });
+      let keywordOptions = filteredOptions.filter(option => {
+         const keywords = KpmKeywords[option.value] || [];
+         return keywords.includes(search);
+      });
+      filteredOptions = _.union(labelOptions, keywordOptions);
     }
     return _.orderBy(filteredOptions, (option: KeyPerformanceMetric) => {
       return option.label;
     }, orderByDir);
   }
-
 }
