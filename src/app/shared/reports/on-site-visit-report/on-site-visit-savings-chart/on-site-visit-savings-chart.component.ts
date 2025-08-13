dc98fe4b0f428be613dee19d2cc014fbadb0f8d6
jamlokim <jamlokim@ornl.gov>
2025-08-13 16:42:17 -0400

    added a pipe for rounding off values

--- src/app/shared/reports/on-site-visit-report/on-site-visit-savings-chart/on-site-visit-savings-chart.component.ts
+++ src/app/shared/reports/on-site-visit-report/on-site-visit-savings-chart/on-site-visit-savings-chart.component.ts
@@ -6,6 +6,7 @@ import { LocaleService } from 'src/app/shared/shared-services/locale.service';
 import { localeCurrency } from 'src/app/shared/constants/localeCurrency';
 import { CurrencySymbolPipe } from 'src/app/shared/helper-pipes/currency-symbol.pipe';
 import { CurrencyPipe } from '@angular/common';
+import { DisplayRoundedValuesPipe } from 'src/app/shared/helper-pipes/display-rounded-values.pipe';
 
 @Component({
   selector: 'app-on-site-visit-savings-chart',
@@ -27,7 +28,8 @@ export class OnSiteVisitSavingsChartComponent {
   currencySymbolPipe: CurrencySymbolPipe;
   constructor(private plotlyService: PlotlyService,
     private localeService: LocaleService,
-    private currencyPipe: CurrencyPipe
+    private currencyPipe: CurrencyPipe,
+    private displayRoundedValuesPipe: DisplayRoundedValuesPipe
   ) {
   }
 
@@ -97,10 +99,11 @@ export class OnSiteVisitSavingsChartComponent {
 
 
       var data = [trace1, trace2];
+      const roundedValue = this.displayRoundedValuesPipe.transform(this.onSiteVisitReport.totalFinancialImpact, true);
       var layout = {
         height: 250,
         title: {
-          text: 'Total Annual Financial Impact<br>' + this.currencyUnicode + this.onSiteVisitReport.totalFinancialImpact.toLocaleString() + ' (' + this.currencyUnicode + '/yr)',
+          text: 'Total Annual Financial Impact<br>' + this.currencyUnicode + roundedValue.toLocaleString() + ' (' + this.currencyUnicode + '/yr)',
           font: {
             weight: 'bold'
           }
