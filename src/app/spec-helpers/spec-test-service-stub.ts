39c9b165b29875158921ef4c9a258be9e4976b44
jamlokim <jamlokim@ornl.gov>
2025-11-12 14:35:11 -0500

    added option to export reports to excel

--- src/app/spec-helpers/spec-test-service-stub.ts
+++ src/app/spec-helpers/spec-test-service-stub.ts
@@ -160,6 +160,7 @@ let sharedDataService: Partial<SharedDataService> = {
     createPowerPoint: new BehaviorSubject<boolean>(false),
     showSlideShow: new BehaviorSubject<boolean>(false),
     exportToExcel: new BehaviorSubject<boolean>(false),
+    exportReportToExcel: new BehaviorSubject<string>(undefined)
 }
 
 let companyContactsFormService: Partial<CompanyContactsFormService> = {
