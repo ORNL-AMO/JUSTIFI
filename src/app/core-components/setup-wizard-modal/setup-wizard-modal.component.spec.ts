2f5a0f252fe33d8d1540ac2d6556cddffe4f4647
Mark Root <rootrm@ornl.gov>
2025-12-01 14:00:51 -0600

    all tests passing

--- src/app/core-components/setup-wizard-modal/setup-wizard-modal.component.spec.ts
+++ src/app/core-components/setup-wizard-modal/setup-wizard-modal.component.spec.ts
@@ -3,13 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SetupWizardModalComponent } from './setup-wizard-modal.component';
 import { FormsModule } from '@angular/forms';
 import { stubServiceProviders } from 'src/app/spec-helpers/spec-test-service-stub';
+import { HelperPipesModule } from 'src/app/shared/helper-pipes/_helper-pipes.module';
 
 describe('SetupWizardModalComponent', () => {
   let component: SetupWizardModalComponent;
   let fixture: ComponentFixture<SetupWizardModalComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormsModule],
+      imports: [FormsModule, HelperPipesModule],
       declarations: [SetupWizardModalComponent],
       providers: stubServiceProviders
     })
