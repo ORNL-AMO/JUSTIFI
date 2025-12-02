08bfa329272cbeb93595a4acfd29086f74daad93
Mark Root <rootrm@ornl.gov>
2025-12-02 08:13:53 -0600

    chore: all tests passing

--- src/app/core-components/update-check/update-check.component.spec.ts
+++ src/app/core-components/update-check/update-check.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateCheckComponent } from './update-check.component';
+import { UpdateCheckService } from './update-check.service';
 
 describe('UpdateCheckComponent', () => {
   let component: UpdateCheckComponent;
   let fixture: ComponentFixture<UpdateCheckComponent>;
+  
+  class UpdateCheckServiceStub {
+    updateAvailable = { subscribe: () => {} };
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UpdateCheckComponent]
+      declarations: [UpdateCheckComponent],
+      providers: [
+        { provide: UpdateCheckService, useClass: UpdateCheckServiceStub }
+      ]
     })
     .compileComponents();
 
--- src/app/core-components/update-check/update-check.service.spec.ts
+++ src/app/core-components/update-check/update-check.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UpdateCheckService } from './update-check.service';
+import { SwUpdate } from '@angular/service-worker';
+import { ElectronService } from 'src/app/electron/electron.service';
+import { of, Subject } from 'rxjs';
 
 describe('UpdateCheckService', () => {
   let service: UpdateCheckService;
 
+
+  class SwUpdateStub {
+    isEnabled = false;
+    versionUpdates = new Subject();
+    checkForUpdate = async () => false;
+  }
+
+  class ElectronServiceStub {
+    isElectron = false;
+  }
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SwUpdate, useClass: SwUpdateStub },
+        { provide: ElectronService, useClass: ElectronServiceStub }
+      ]
+    });
     service = TestBed.inject(UpdateCheckService);
   });
 
