14d8c9cdb5c026473bd02e25aa6295e36bf0a709
jamlokim <jamlokim@ornl.gov>
2026-09-24 09:59:52 -0400

    added pagination and sorting to on-site visits table

--- src/app/shared/table-pagination/table-pagination.component.ts
+++ src/app/shared/table-pagination/table-pagination.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 
+type PageNumberOption = number | 'ellipsis';
+
 @Component({
     selector: 'app-table-pagination',
     templateUrl: './table-pagination.component.html',
@@ -16,7 +18,7 @@ export class TablePaginationComponent {
   @Output('emitPageNumber')
   emitPageNumber: EventEmitter<number> = new EventEmitter<number>();
 
-  pageNumberOptions: Array<number>;
+  pageNumberOptions: Array<PageNumberOption> = [];
   numberOfPages: number;
 
   ngOnInit() {
@@ -24,22 +26,56 @@ export class TablePaginationComponent {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    //update values on collection size change or items per page change
-    if ((changes['collectionSize'] && !changes['collectionSize'].firstChange) || (changes['itemsPerPage'] && !changes['itemsPerPage'].firstChange)) {
+    if (changes['pageNumber'] || changes['collectionSize'] || changes['itemsPerPage']) {
       this.setValues();
     }
   }
 
   setValues() {
     this.numberOfPages = Math.ceil(this.collectionSize / this.itemsPerPage);
-    this.pageNumberOptions = new Array();
-    for (let i = 1; i <= this.numberOfPages; i++) {
-      this.pageNumberOptions.push(i);
-    }
+    this.pageNumber = Math.min(Math.max(this.pageNumber || 1, 1), Math.max(this.numberOfPages, 1));
+    this.pageNumberOptions = this.getPageNumberOptions();
   }
 
   setPageNumber(pageNumber: number) {
+    if (pageNumber < 1 || pageNumber > this.numberOfPages || pageNumber === this.pageNumber) {
+      return;
+    }
+
     this.pageNumber = pageNumber;
+    this.pageNumberOptions = this.getPageNumberOptions();
     this.emitPageNumber.emit(this.pageNumber);
+  }
+
+  private getPageNumberOptions(): Array<PageNumberOption> {
+    if (this.numberOfPages <= 7) {
+      return Array.from({ length: this.numberOfPages }, (_, index) => index + 1);
+    }
+
+    if (this.pageNumber <= 4) {
+      return [1, 2, 3, 4, 5, 'ellipsis', this.numberOfPages];
+    }
+
+    if (this.pageNumber >= this.numberOfPages - 3) {
+      return [
+        1,
+        'ellipsis',
+        this.numberOfPages - 4,
+        this.numberOfPages - 3,
+        this.numberOfPages - 2,
+        this.numberOfPages - 1,
+        this.numberOfPages
+      ];
+    }
+
+    return [
+      1,
+      'ellipsis',
+      this.pageNumber - 1,
+      this.pageNumber,
+      this.pageNumber + 1,
+      'ellipsis',
+      this.numberOfPages
+    ];
   }
 }
